899d365927d2ff833e95e4921b2471b5bd666f56
DeCri0t <yurapzx@gmail.com>
2023-11-21 17:37:47 +0500

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 import { generateRandomComment, getRandomInt } from './util.js';
 //Возможно лишнее сейчас, тк импорт для createPhotosData написан в data.js
 import { createPhotosData } from './data.js';
+import { PictureModule } from './thumbnail.js';
 
 const photos = createPhotosData();
+
+PictureModule.renderPictures(picturesData);
 
 console.log(photos);
