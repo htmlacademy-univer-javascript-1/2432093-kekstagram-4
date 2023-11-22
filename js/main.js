06f258cbb77d22baf78b048c247cdbc7be8b9aa9
DeCri0t <yurapzx@gmail.com>
2023-11-22 12:09:05 +0500

    Исправил по коментариям

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 //Файл - точка входа
-import { generateRandomComment, getRandomInt } from './util.js';
+//Данный импорт нужен был в прошлом задании
+//import { generateRandomComment, getRandomInt } from './util.js';
 //Возможно лишнее сейчас, тк импорт для createPhotosData написан в data.js
 import { createPhotosData } from './data.js';
 import { PictureModule } from './thumbnail.js';
 
 const photos = createPhotosData();
 
-PictureModule.renderPictures(picturesData);
+PictureModule.renderPictures(photos);
 
 console.log(photos);
