b8cc5d667ae3a11ee2ebf90bd39485fedb5153f5
DeCri0t <yurapzx@gmail.com>
2023-11-28 22:00:36 +0500

    Добавил и поправил

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 //import { generateRandomComment, getRandomInt } from './util.js';
 //Возможно лишнее сейчас, тк импорт для createPhotosData написан в data.js
 import { createPhotosData } from './data.js';
-import { PictureModule } from './thumbnail.js';
+import { PictureModule } from './trumbnail.js';
 import { FullScreenModule } from './fullScreen.js';
 
 const photos = createPhotosData();
 
-PictureModule.renderPictures(photos);
+PictureModule().renderPictures(photos);
 
 const thumbnailElements = document.querySelectorAll('.picture');
 thumbnailElements.forEach((thumbnail, index) => {
