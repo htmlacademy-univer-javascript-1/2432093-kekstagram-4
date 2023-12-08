766a02062b296e0a48f7fb2df0086bf55567e833
Keks <keks@htmlacademy.ru>
2023-12-08 11:19:35 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,20 @@
+//Файл - точка входа
+//Данный импорт нужен был в прошлом задании
+//import { generateRandomComment, getRandomInt } from './util.js';
+//Возможно лишнее сейчас, тк импорт для createPhotosData написан в data.js
+import { createPhotosData } from './data.js';
+import { FullScreenModule } from './fullScreen.js';
+import { PictureModule } from './thumbnail.js';
+import './add-form.js';
+import './scale.js';
+
+const photos = createPhotosData();
+
+PictureModule().renderPictures(photos);
+
+const thumbnailElements = document.querySelectorAll('.picture');
+thumbnailElements.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', () => {
+    FullScreenModule.openFullScreen(photos[index]);
+  });
+});
---
+++ 9/js/thumbnail.js
@@ -0,0 +1,35 @@
+// Модуль отвечающий за отрисовку миниатюр
+export const PictureModule = (function () {
+  // Функция для создания DOM-элемента фотографии
+  function createPictureElement(pictureData) {
+    const pictureTemplate = document.querySelector('#picture').content.cloneNode(true);
+    const pictureElement = pictureTemplate.querySelector('.picture');
+
+    // Заполнение данными
+    const imgElement = pictureElement.querySelector('.picture__img');
+    imgElement.src = pictureData.url;
+    imgElement.alt = pictureData.description;
+
+    pictureElement.querySelector('.picture__likes').textContent = pictureData.likes;
+    pictureElement.querySelector('.picture__comments').textContent = pictureData.comments.lenght;
+
+    return pictureElement;
+  }
+
+  // Функция для отрисовки фотографий в блок .pictures
+  function renderPictures(picturesData) {
+    const fragment = document.createDocumentFragment();
+    const picturesContainer = document.querySelector('.pictures');
+
+    picturesData.forEach((pictureData) => {
+      const pictureElement = createPictureElement(pictureData);
+      fragment.appendChild(pictureElement);
+    });
+
+    picturesContainer.appendChild(fragment);
+  }
+
+  return {
+    renderPictures: renderPictures,
+  };
+});
